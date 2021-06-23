35132540b676ba43a3c64f910313da898655cd09
michaelfyc <45136049+michaelfyc@users.noreply.github.com>
2021-06-23 22:52:45 +0800

    Update table charsets to utf8mb4
    Hope this won't cause crashes.

--- cnki.sql
+++ cnki.sql
@@ -1,32 +1,29 @@
 drop table if exists user_dynamic;
-create table user_dynamic
-(
-    dynamic_id bigint comment '动态唯一id',
-    type_id    int comment '动态类型, 用于获取评论',
-    oid        bigint comment '动态对象id，用于获取评论',
-    status     int comment '评论爬取状态，0: 未爬取， 1: 至少爬取完成过一次',
-    primary key (dynamic_id)
-) COMMENT '';
+CREATE TABLE `user_dynamic` (
+  `dynamic_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `type_id` int(11) DEFAULT NULL,
+  `oid` bigint(20) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  PRIMARY KEY (`dynamic_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 drop table if exists kv;
-create table kv_store
-(
-    field_name  varchar(255) NOT NULL,
-    field_value text         NOT NULL,
-    primary key (field_name)
-) COMMENT ''
+CREATE TABLE `kv_store` (
+  `field_name` varchar(255) NOT NULL,
+  `field_value` text,
+  PRIMARY KEY (`field_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 drop table if exists reply;
-create table reply
-(
-    rpid      bigint comment '回复id',
-    type_id   int,
-    mid       int comment '成员id',
-    oid       bigint comment '源稿件id',
-    ctime     int '创建时间',
-    m_name    varchar(255) '昵称',
-    content   text '评论内容',
-    like_num  int,
-    json_text text comment '爬虫原始数据',
-    primary key (rpid)
-)
+CREATE TABLE `reply` (
+  `rpid` bigint(20) NOT NULL AUTO_INCREMENT,
+  `type_id` int(11) DEFAULT NULL,
+  `mid` int(11) DEFAULT NULL,
+  `oid` bigint(20) DEFAULT NULL,
+  `ctime` int(11) DEFAULT NULL,
+  `m_name` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `content` text CHARACTER SET utf8,
+  `like_num` int(11) DEFAULT NULL,
+  `json_text` text CHARACTER SET utf8,
+  PRIMARY KEY (`rpid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
